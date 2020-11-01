c0ae045928965f5fe463e593e1cb6ccaa7d71c1f
abdxax <abdxax@gmail.com>
2020-11-01 23:13:26 +0300

    first
    first

---
+++ besh/login.php
@@ -0,0 +1,56 @@
+<?php
+?>
+<html>
+<head>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+<body >
+
+<div class="row">
+    <div class="col-md-6 left-login"></div>
+
+    <div class="col-md-6">
+        <div class="container">
+            <div class="col-12 mt-5">
+                <div class="col-7 text-center">
+                    <img src="bais.png" class="mx-auto d-block">
+
+                </div>
+
+                <div class="text-center">
+                    <h2>المكتبة الالكترونية </h2>
+                </div>
+
+                   <div class="row justify-content-center">
+
+                       <form class="">
+                           <div class="form-group ">
+                               <input type="text" class="form-control"  name="id">
+                           </div>
+
+                           <div class="form-group ">
+                               <input type="password" class="form-control"  name="pass">
+                           </div>
+
+                           <div class="form-group  text-center">
+                               <input type="submit" class="btn btn-info btn-center"  name="sub" value="تسجيل دخول ">
+                           </div>
+
+                       </form>
+                   </div>
+
+                </div>
+
+        </div>
+    </div>
+
+</div>
+
+<!-- JS bootstap-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
